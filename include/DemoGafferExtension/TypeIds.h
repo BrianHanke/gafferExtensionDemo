bb18ec0b1dbcf11c7fefa9b36bf16f8373a82442
Brian R Hanke <brianrhanke@outlook.com>
2026-07-20 14:15:47 -0400

    stuff

--- include/DemoGafferExtension/TypeIds.h
+++ include/DemoGafferExtension/TypeIds.h
@@ -7,8 +7,8 @@ enum class TypeId {
     // DemoSceneProcessorTypeId = 120001,
     // LastTypeId = 120499,
 
-    FirstTypeId = 115000,
-    DemoSceneProcessorTypeId = 115001,
-    LastTypeId = 115499,
+    FirstTypeId = 400000,
+    DemoSceneProcessorTypeId = 400001,
+    LastTypeId = 400499,
 };
 } // namespace DemoGafferExtension
